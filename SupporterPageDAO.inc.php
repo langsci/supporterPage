bacac7c1bd1c58d288474f270f448091c12376f7
root <cfanselow>
2016-06-29 01:25:31 +0200

    type variable added

--- SupporterPageDAO.inc.php
+++ SupporterPageDAO.inc.php
@@ -31,14 +31,14 @@ class SupporterPageDAO extends DAO {
 		} else {
 			$row = $result->getRowAssoc(false);
 			$result->Close();
-			return $this->convertFromDB($row['user_group_id']);
+			return $this->convertFromDB($row['user_group_id'],null);
 		}	
 	}
 
 	function getSupporters($supporterGroupId,$locale) {
 
 		$result = $this->retrieve(
-			"select a.user_id, a.salutation, a.first_name, a.last_name, a.url, c.setting_value as affiliation, b.user_group_id from users a left join user_user_groups b left join user_settings c on b.user_id=c.user_id on b.user_id=a.user_id where b.user_group_id=".$supporterGroupId." and c.locale='".$locale."' and c.setting_name='affiliation'"
+			"SELECT a.user_id, a.salutation, a.first_name, a.last_name, a.url, c.setting_value AS affiliation, b.user_group_id FROM users a LEFT JOIN user_user_groups b LEFT JOIN user_settings c ON b.user_id=c.user_id on b.user_id=a.user_id WHERE b.user_group_id=".$supporterGroupId." AND c.locale='".$locale."' AND c.setting_name='affiliation'"
 		);
 
 		if ($result->RecordCount() == 0) {
@@ -49,12 +49,12 @@ class SupporterPageDAO extends DAO {
 			$i = 0;
 			while (!$result->EOF) {
 				$row = $result->getRowAssoc(false);
-				$supporters[$i]['id'] = $this->convertFromDB($row['user_id']);
-				$supporters[$i]['salutation'] = $this->convertFromDB($row['salutation']);
-				$supporters[$i]['firstName'] = $this->convertFromDB($row['first_name']);
-				$supporters[$i]['lastName'] = $this->convertFromDB($row['last_name']);
-				$supporters[$i]['url'] = $this->convertFromDB($row['url']);
-				$supporters[$i]['affiliation'] = $this->convertFromDB($row['affiliation']);
+				$supporters[$i]['id'] = $this->convertFromDB($row['user_id'],null);
+				$supporters[$i]['salutation'] = $this->convertFromDB($row['salutation'],null);
+				$supporters[$i]['firstName'] = $this->convertFromDB($row['first_name'],null);
+				$supporters[$i]['lastName'] = $this->convertFromDB($row['last_name'],null);
+				$supporters[$i]['url'] = $this->convertFromDB($row['url'],null);
+				$supporters[$i]['affiliation'] = $this->convertFromDB($row['affiliation'],null);
 				$i++;		 
 				$result->MoveNext();
 			}
@@ -66,7 +66,7 @@ class SupporterPageDAO extends DAO {
 	function getProminentSupporters() {
 
 		$result = $this->retrieve(
-			"select user_id from langsci_prominent_supporters");
+			"SELECT user_id FROM langsci_prominent_supporters");
 
 		if ($result->RecordCount() == 0) {
 			$result->Close();
@@ -75,7 +75,7 @@ class SupporterPageDAO extends DAO {
 			$prominentUsers = array();
 			while (!$result->EOF) {
 				$row = $result->getRowAssoc(false);
-				$prominentUsers[] = $this->convertFromDB($row['user_id']);	 
+				$prominentUsers[] = $this->convertFromDB($row['user_id'],null);	 
 				$result->MoveNext();
 			}
 			$result->Close();
@@ -86,7 +86,7 @@ class SupporterPageDAO extends DAO {
 	function getProminentSupportersUsernames() {
 
 		$result = $this->retrieve(
-			"select b.username from langsci_prominent_supporters a left join users b on a.user_id=b.user_id order by b.username");
+			"SELECT b.username FROM langsci_prominent_supporters a LEFT JOIN users b ON a.user_id=b.user_id ORDER BY b.username");
 
 		if ($result->RecordCount() == 0) {
 			$result->Close();
@@ -95,7 +95,7 @@ class SupporterPageDAO extends DAO {
 			$prominentUsers = array();
 			while (!$result->EOF) {
 				$row = $result->getRowAssoc(false);
-				$prominentUsers[] = $this->convertFromDB($row['username']);	 
+				$prominentUsers[] = $this->convertFromDB($row['username'],null);	 
 				$result->MoveNext();
 			}
 			$result->Close();
@@ -105,32 +105,32 @@ class SupporterPageDAO extends DAO {
 
 	function addProminentUser($username) {
 
-		$result = $this->retrieve("select user_id from users where username='".$username."'");
+		$result = $this->retrieve("SELECT user_id FROM users WHERE username='".$username."'");
 
 		if ($result->RecordCount() == 0) {
 			$result->Close();	 
 			return false;
 		} else {
 			$row = $result->getRowAssoc(false);
-			$userId = $this->convertFromDB($row['user_id']);
+			$userId = $this->convertFromDB($row['user_id'],null);
 			$result->Close();
- 			$this->update('insert into langsci_prominent_supporters (user_id) values('.$userId.')');
+ 			$this->update('INSERT INTO langsci_prominent_supporters (user_id) VALUES('.$userId.')');
 			return true;
 		}
 	}
 
 	function removeProminentUser($username) {
 
-		$result = $this->retrieve("select user_id from users where username='".$username."'");
+		$result = $this->retrieve("SELECT user_id FROM users WHERE username='".$username."'");
 
 		if ($result->RecordCount() == 0) {
 			$result->Close();	 
 			return false;
 		} else {
 			$row = $result->getRowAssoc(false);
-			$userId = $this->convertFromDB($row['user_id']);
+			$userId = $this->convertFromDB($row['user_id'],null);
 			$result->Close();
- 			$this->update('delete from langsci_prominent_supporters where user_id='.$userId);
+ 			$this->update('DELETE FROM langsci_prominent_supporters WHERE user_id='.$userId);
 			return true;
 		}
 	}
